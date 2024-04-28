1deb6e90cf54447f60d6c0d23d98ef34c1f85503
rajashreedebnath <rajashreedebnath29@gmail.com>
2024-04-28 07:08:45 +0530

    Test

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 
-function App() {
+export default function Stopwatch() {
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    
+    let timer;
+    if(isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  
+
+  const formatTime = () => {
+
+    const min = Math.floor(time/60).toString().padStart(1, "0");
+    const sec = (time%60).toString().padStart(2, "0");
+
+    return `${min}:${sec}`;
+
+  };
+
+
+  const startStopWatch = () => {
+    setIsRunning(true);
+  };
+
+  const stopStopWatch = () => {
+    setIsRunning(false);
+  }
+
+  const resetStopWatch = () => {
+
+    setTime(0);
+    setIsRunning(false);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="stopwatch">
+
+      <h2>Stopwatch</h2>
+
+      <div className="timer">
+        Time: {formatTime()}
+      </div>
+      
+      <div>
+        
+        {isRunning ? (
+          <button onClick = {stopStopWatch}>Stop</button>
+        ) : (
+          <button onClick = {startStopWatch}>Start</button>
+        )
+        }
+
+        <button onClick = {resetStopWatch}>Reset</button>
+
+      </div>
     </div>
   );
 }
 
-export default App;
+
